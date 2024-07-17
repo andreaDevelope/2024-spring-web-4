09e9e6f9c70e3f38bc84332d2f30f11a2ce1c829
andreaDevelope <andrea_develope@outlook.it>
2024-07-17 16:16:59 +0200

    aggiunta di view

--- src/main/java/org/java/spring_web4/web/controller/MainFarmerController.java
+++ src/main/java/org/java/spring_web4/web/controller/MainFarmerController.java
@@ -13,6 +13,7 @@ import org.java.spring_web4.web.dto.FarmerDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/v1/farmers")
+@CrossOrigin(origins = "http://localhost:5173")
 public class MainFarmerController {
     @Autowired
     private FarmerService farmerService;
