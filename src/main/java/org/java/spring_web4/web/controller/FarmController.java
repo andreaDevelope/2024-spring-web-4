bc4c3c5a8e1204f7b31f803eb46590272b28c27c
andreaDevelope <andrea_develope@outlook.it>
2024-07-18 18:11:57 +0200

    aggiornamento 18/07/2024

--- src/main/java/org/java/spring_web4/web/controller/FarmController.java
+++ src/main/java/org/java/spring_web4/web/controller/FarmController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/farms")
+@CrossOrigin(origins = "http://localhost:5173")
 public class FarmController {
     @Autowired
     private FarmService farmService;
